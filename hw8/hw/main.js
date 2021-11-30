b8fc44d879e639bb7ad82f21d4a975f7456b2a0a
Taras-Kvas <92385495+Taras-Kvas@users.noreply.github.com>
2021-11-30 17:15:42 +0200

    Add files via upload

---
+++ hw8/hw/main.js
@@ -0,0 +1,56 @@
+// Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
+// -- отримує текст з параграфа з id "content"
+// -- отримує текст з блоку з id "rules"
+// -- замініть текст параграфа з id 'content' на будь-який інший
+// -- замініть текст параграфа з id 'rules' на будь-який інший
+// -- змініть кожному елементу колір фону на червоний
+// -- змініть кожному елементу колір тексту на синій
+// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+// -- поміняти колір тексту у всіх елементів fc_rules на червоний
+
+// let elementById = document.getElementById('content');
+// console.log(elementById);
+
+
+// let elementById1 = document.getElementById('rules');
+// console.log(elementById1);
+
+
+// let elementById = document.getElementById('content');
+// elementById.innerText = 'taras taras taras'
+// console.log(elementById);
+
+// let elementById = document.getElementById('rules');
+// elementById.innerText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, tenetur.'
+// console.log(elementById);
+
+// let byTagNameNS = document.getElementsByTagName('div');
+// for (const byTagNameN of byTagNameNS) {
+//     console.log(byTagNameNS)
+//     byTagNameN.style.background = 'red';
+//     byTagNameN.style.color = 'blue'
+// }
+// let byTagNameNS1 = document.getElementsByTagName('p');
+// for (const byTagNameN of byTagNameNS1) {
+//     console.log(byTagNameNS1)
+//     byTagNameN.style.background = 'red';
+//     byTagNameN.style.color = 'blue'
+// }
+// let byTagNameNS2 = document.getElementsByTagName('ul');
+// for (const byTagNameN of byTagNameNS2) {
+//     console.log(byTagNameNS2)
+//     byTagNameN.style.background = 'red';
+//     byTagNameN.style.color = 'blue'
+// }
+
+// let elementById = document.getElementById('rules');
+// console.log(elementById);
+
+
+// let elemntsList = document.getElementsByClassName('fc_rules');
+// for (const elemntsListElement of elemntsList) {
+//     console.log(elemntsList)
+//     elemntsListElement.style.background = 'red';
+// }
+
+
